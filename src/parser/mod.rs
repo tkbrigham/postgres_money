573c3dc2fcc799160f094febccfe0c958bc97d7f
Thomas Brigham <thomas@thomasbrigham.me>
2020-04-25 10:30:24 -0700

    Refactor into struct

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -29,13 +29,82 @@ impl Money {
     // }
 }
 
-// fn unwrap_minus(input: &str) -> Option<Amount> {
-//     let neg = Regex::new(r"^-\$(?P<cash_neg>[\d\.]*)").unwrap();
-//     for cap in neg.captures_iter(input) {
-//         return Some(Amount::Negative(cap["cash_neg"].to_string()))
-//     }
-//     None
-// }
+type Inner = String;
+
+#[derive(Clone, Eq, Hash, Ord, PartialEq, PartialOrd, Debug)]
+enum AmountKind {
+    Negative,
+    Positive,
+}
+
+#[derive(Clone, Eq, Hash, Ord, PartialEq, PartialOrd, Debug)]
+struct Amount {
+    kind: AmountKind,
+    inner: Inner
+}
+
+impl Amount {
+    fn new(kind: AmountKind, inner: &str) -> Result<Self, Error> {
+        return if Self::valid_inner().is_match(inner) {
+            Ok(Amount {
+                kind,
+                inner: inner.to_string()
+            })
+        } else {
+            Err(Error::InvalidString)
+        }
+    }
+
+    pub fn positive(s: &str) -> Result<Amount, Error> {
+        Self::new(AmountKind::Positive, s)
+    }
+
+    pub fn negative(s: &str) -> Result<Amount, Error> {
+        Self::new(AmountKind::Negative, s)
+    }
+
+    fn valid_inner() -> Regex {
+        Regex::new(r"^\$?(?P<dollars>\d*)\.?(?P<cents>\d*$)").unwrap()
+    }
+
+    fn from(s: &str) -> Result<Self, Error> {
+        let has_minus = Regex::new(r"-(.*)").unwrap();
+        let has_paren = Regex::new(r"\((.*)\)").unwrap();
+
+        let m: Vec<Regex> = vec![has_minus, has_paren].into_iter()
+            .filter(|r| r.is_match(s))
+            .collect();
+
+        return match m.len() {
+            0 => Self::positive(s),
+            1 => {
+                let transformed = m.into_iter()
+                    .fold(s, |s, r| r.captures(s).unwrap().get(1).unwrap().as_str());
+                Self::negative(transformed)
+            },
+            _ => Err(Error::InvalidString)
+        }
+    }
+
+    // fn to_money(self) -> Money {
+    //
+    // }
+
+    // fn combine_dollars_and_cents(&self) -> i64 {
+    //
+    // }
+
+    // fn get_parts(self) {
+    //     let caps = Self::valid_inner().captures(self.0).unwrap();
+    //
+    //     let dollars = caps.get(1).map_or("", |m| m.as_str());
+    //     let cents = caps.get(2).map_or("", |m| m.as_str());
+    //
+    //     println!("dollars = {:?}", dollars);
+    //     println!("cents = {:?}", cents);
+    //     // (dollars, cents)
+    // }
+}
 
 // expects no currency symbols
 fn money_str_to_int(input: &str) -> Result<i64, Error> {
@@ -50,64 +119,117 @@ fn mk_int(s: &str) -> Result<i64, Error> {
     str::parse::<i64>(&s).map_err(|_e| Error::ParseInt)
 }
 
-// fn unwrap_paren(input: &str) -> Option<Amount> {
+// fn cents_int_for(s: &str) -> i8 {
 //
 // }
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::parser::error::Error::InvalidString;
 
     #[test]
-    fn test_valid_123_45() {
-        assert_eq!(Money::parse_str("$123.45"), Ok(Money(12345)))
+    fn test_goof() {
+        let a = Amount::positive("100").unwrap();
+        println!("{}", a.inner);
     }
 
     // #[test]
+    // fn test_amt_pos() {
+    //     let a = Amount::from("100");
+    //     assert_eq!(a, Ok(Amount::positive("100")));
+    // }
+    //
+    // #[test]
+    // fn test_amt_pos_2() {
+    //     let a = Amount::from("$100");
+    //     assert_eq!(a, Ok(Amount::positive("$100")));
+    // }
+    //
+    // #[test]
+    // fn test_amt_invalid() {
+    //     let a = Amount::from("-($100)");
+    //     assert!(a.is_err());
+    // }
+    //
+    // #[test]
+    // fn test_amt_neg_paren() {
+    //     let a = Amount::from("-$100")?;
+    //     assert_eq!(a, Amount::negative("$100"));
+    // }
+    //
+    // #[test]
+    // fn test_wacky() {
+    //     let a = Amount::from("-$100-$100");
+    //     assert_eq!(a, Err(Error::InvalidString));
+    // }
+
+    // #[test]
+    // fn test_amt_neg_minus() {
+    //     let a = Amount::from("($100)");
+    //     assert_eq!(a, Ok(Amount::Negative("100".to_string())));
+    // }
+
+    // #[test]
+    // fn test_neg_parts() {
+    //     let s = "-1000.0";
+    //
+    //     get_parts(s);
+    // }
+    //
+    //
+    // #[test]
+    // fn test_valid_123_45() {
+    //     assert_eq!(Money::parse_str("$123.45"), Ok(Money(12345)))
+    // }
+    //
+    // #[test]
     // fn test_valid_123_451() {
-    //     assert_eq!(Money::parse_str("$123.451"), Money(12345))
+    //     assert_eq!(Money::parse_str("$123.451"), Ok(Money(12345)))
     // }
     //
     // #[test]
     // fn test_valid_123_454() {
-    //     assert_eq!(Money::parse_str("$123.454"), Money(12345))
+    //     assert_eq!(Money::parse_str("$123.454"), Ok(Money(12345)))
     // }
     //
     // #[test]
     // fn test_valid_123_455() {
-    //     assert_eq!(Money::parse_str("$123.455"), Money(12346))
+    //     assert_eq!(Money::parse_str("$123.455"), Ok(Money(12346)))
     // }
     //
     // #[test]
     // fn test_valid_123_456() {
-    //     assert_eq!(Money::parse_str("$123.456"), Money(12346))
+    //     assert_eq!(Money::parse_str("$123.456"), Ok(Money(12346)))
     // }
     //
     // #[test]
     // fn test_valid_123_459() {
-    //     assert_eq!(Money::parse_str("$123.459"), Money(12346))
+    //     assert_eq!(Money::parse_str("$123.459"), Ok(Money(12346)))
     // }
     //
     // #[test]
     // fn test_valid_1234567890() {
-    //     assert_eq!(Money::parse_str("1234567890"), Money(123456789000))
+    //     assert_eq!(Money::parse_str("1234567890"), Ok(Money(123456789000)))
     // }
     //
     // #[test]
     // fn test_valid_12345678901234567() {
-    //     assert_eq!(Money::parse_str("12345678901234567"), Money(1234567890123456700))
+    //     assert_eq!(Money::parse_str("12345678901234567"), Ok(Money(1234567890123456700)))
     // }
     //
     // #[test]
     // fn test_invalid_123456789012345678() {
-    //     assert_eq!(Money::parse_str("123456789012345678"), Error(OutOfRange))
+    //     assert_eq!(Money::parse_str("123456789012345678"), Err(Error::OutOfRange))
     // }
     //
     // #[test]
     // fn test_invalid_9223372036854775807() {
-    //     assert_eq!(Money::parse_str("9223372036854775807"), Error(OutOfRange))
+    //     assert_eq!(Money::parse_str("9223372036854775807"), Err(Error::OutOfRange))
     // }
-    //
+
     // #[test]
     // fn test_valid_neg_12345() {
     //     assert_eq!(Money::parse_str("-12345"), Money(-1234500))
